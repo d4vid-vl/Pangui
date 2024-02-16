510814d7615548246837f233172a9dc4b5e9fc56
pauzeql <david.verdugo.lizana@gmail.com>
2024-02-16 14:47:13 -0300

    added commands

--- src/framework/options/commands/mod.rs
+++ src/framework/options/commands/mod.rs
@@ -1,10 +1,13 @@
 mod info;
+mod horario;
 
 use poise::Command;
 use crate::{Data, Error};
 
 pub async fn guild_commands() -> Vec<Command<Data, Error>>{
     vec![
-        info::info::info()
+        info::info::info(),
+        horario::revisar_curso::revisar_curso(),
+        horario::revisar_sigla::revisar_sigla()
     ]
 }
