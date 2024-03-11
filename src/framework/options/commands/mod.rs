bbcae9de9fd2bdfedd401818a3b5fb1c1ca31508
pauzeql <david.verdugo.lizana@gmail.com>
2024-03-10 22:19:47 -0300

    implement spotify module

--- src/framework/options/commands/mod.rs
+++ src/framework/options/commands/mod.rs
@@ -1,15 +1,16 @@
-mod info;
 mod horario;
+mod info;
+mod spotify;
 
-use poise::Command;
 use crate::{Data, Error};
+use poise::Command;
 
-pub async fn guild_commands() -> Vec<Command<Data, Error>>{
+pub async fn guild_commands() -> Vec<Command<Data, Error>> {
     vec![
         info::info::info(),
         horario::revisar_nombre::revisar_nombre(),
         horario::revisar_sigla::revisar_sigla(),
         horario::revisar_nrc::revisar_nrc(),
-        horario::revisar_profesor::revisar_profesor()
+        horario::revisar_profesor::revisar_profesor(),
     ]
 }
