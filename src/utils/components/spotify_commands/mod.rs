d5528ee20e95b61ab2c5f25c59956b5467f4c0d9
pauzeql <david.verdugo.lizana@gmail.com>
2024-03-12 22:00:17 -0300

    added structs for data parsing

--- src/utils/components/spotify_commands/mod.rs
+++ src/utils/components/spotify_commands/mod.rs
@@ -1,10 +1,31 @@
 pub mod spotify_artists;
 
 use dotenv::dotenv;
-use std::env;
+use serde::{Deserialize, Serialize};
 use spotify_rs::{ClientCredsClient, ClientCredsFlow};
+use std::env;
 
-async fn spotify_token() -> Result<String, Box<dyn std::error::Error>>{
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Artist {
+    pub name: String,
+    pub id: String,
+    pub genres: Vec<String>,
+    pub popularity: u32,
+    pub followers: u32,
+    pub external_urls: ExternalUrls,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Items<T> {
+    pub items: Vec<T>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct ExternalUrls {
+    pub spotify: String,
+}
+
+async fn spotify_token() -> Result<String, Box<dyn std::error::Error>> {
     dotenv().expect("Expected env");
     let client_id = env::var("SPOTIFY_ID").expect("missing SPOTIFY_ID");
     let client_secret = env::var("SPOTIFY_SECRET").expect("missing SPOTIFY_SECRET");
