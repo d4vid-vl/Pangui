c1c5981be0e72f1a9b70e0255733fc7a4b60d229
pauzeql <david.verdugo.lizana@gmail.com>
2024-03-13 22:06:38 -0300

    TODO: Fix errors in the handling of parameters, look at variables and the command

--- src/utils/components/spotify_commands/mod.rs
+++ src/utils/components/spotify_commands/mod.rs
@@ -1,17 +1,50 @@
 pub mod spotify_artists;
+pub mod spotify_lps;
 
 use dotenv::dotenv;
 use serde::{Deserialize, Serialize};
 use spotify_rs::{ClientCredsClient, ClientCredsFlow};
 use std::env;
 
+// TODO: Fixear errores al pedir álbumes, no recibe ciertos parámetros
+
+// Structs generales
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Artist {
     pub name: String,
     pub id: String,
-    pub genres: Vec<String>,
-    pub popularity: u32,
-    pub followers: u32,
+    pub genres: Option<Vec<String>>,
+    pub images: Option<Vec<Images>>,
+    pub popularity: Option<u32>,
+    pub followers: Option<Followers>,
+    pub external_urls: ExternalUrls,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Album {
+    pub name: String,
+    pub id: String,
+    pub artists: Vec<Artist>,
+    pub release_date: String,
+    pub album_type: String,
+    pub total_tracks: u32,
+    pub images: Vec<Images>,
+    pub genres: Option<Vec<String>>,
+    pub tracks: Option<Vec<Track>>,
+    pub label: Option<String>,
+    pub copyrights: Option<Vec<Copyrights>>,
+    pub external_urls: Option<ExternalUrls>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Track {
+    pub name: String,
+    pub id: String,
+    pub artists: Vec<Artist>,
+    pub album: Album,
+    pub duration_ms: u32,
+    pub genres: Option<Vec<String>>,
+    pub explicit: bool,
     pub external_urls: ExternalUrls,
 }
 
@@ -20,9 +53,27 @@ pub struct Items<T> {
     pub items: Vec<T>,
 }
 
+// Structs específicos
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Images {
+    pub url: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Followers {
+    pub href: Option<String>,
+    pub total: u32,
+}
+
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct ExternalUrls {
     pub spotify: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Copyrights {
+    pub text: String,
+    pub type_: Option<String>,
 }
 
 async fn spotify_token() -> Result<String, Box<dyn std::error::Error>> {
